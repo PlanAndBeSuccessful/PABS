0d0f5ca7acee60ec60ad7eb39a091c9939b35414
r_arnold_r <rozsnyaiarnoldrobert@yahoo.com>
2020-10-15 16:25:27 +0300

    update 0.2
    10/15/2020
    Added features:
    - Fragments package
    - NicknameDialogFragment
    - fragment_dialog_nickname.xml
    
    Updated:
    -LoginActivity(nickname)

---
+++ app/src/main/java/com/example/pabs/Fragments/NicknameDialogFragment.java
@@ -0,0 +1,82 @@
+package com.example.pabs.Fragments;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatDialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.example.pabs.R;
+
+
+public class NicknameDialogFragment extends AppCompatDialogFragment {
+
+    //edittext
+    private EditText nickname_et = null;
+    private NicknameDialogListener nicknameDialogListener;
+
+    /**
+     * When the dialog is created
+     */
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        //setting view
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.fragment_dialog_nickname, null);
+
+        //building the custom dialog fragment
+        builder.setView(view)
+                .setTitle("Nickname")
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String nickname = nickname_et.getText().toString();
+                        //calling interface to set the nickname
+                        nicknameDialogListener.applyNickname(nickname);
+                    }
+                });
+
+        nickname_et = view.findViewById(R.id.d_n_nickname);
+
+        return builder.create();
+    }
+
+    /**
+     * when the NicknameDialogListener is implemented in another class
+     */
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        try {
+            nicknameDialogListener = (NicknameDialogListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() +
+                "Must implement NicknameDialogListener");
+        }
+    }
+
+    /**
+     * interface to set the nickname
+     */
+    public interface NicknameDialogListener{
+        void applyNickname(String nickname);
+    }
+}
--- app/src/main/java/com/example/pabs/LoginActivity.java
+++ app/src/main/java/com/example/pabs/LoginActivity.java
@@ -2,8 +2,13 @@ package com.example.pabs;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,6 +16,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.pabs.Fragments.NicknameDialogFragment;
 import com.example.pabs.Models.User;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.ChildEventListener;
@@ -20,15 +26,17 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements NicknameDialogFragment.NicknameDialogListener {
 
     //UI
     private EditText name_et = null, password_et = null;
     private Button forgot_password_btn = null, login_btn = null, register_btn = null;
+    private String nickname = null;
+   // private Object currentUser = null;
 
     //firebase
     DatabaseReference reference;
-
+    String currentUser;
     /**
      * on create
      */
@@ -77,60 +85,129 @@ public class LoginActivity extends AppCompatActivity {
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final String name = name_et.getText().toString();
-                final String password = password_et.getText().toString();
-
-                //reference to all the ID's of USER
-                reference.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        boolean login_successful = false;
-                        //iterate through ID elements of USER
-                        for (DataSnapshot child : snapshot.getChildren()) {
-                            //examine if user credentials are correct
-                            if((child.child("user_name").getValue().toString().equals(name)) && (child.child("password").getValue().toString().equals(password))){
-                                login_successful = true;
-                            }
-                        }
-
-                        if(login_successful){
-                            //if login was successful
-                            LoginSuccessful();
-                        }
-                        else
-                        {
-                            //if login failed
-                            Toast.makeText(LoginActivity.this, "Login failed!", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                    }
-                });
+                //check internet connection
+                if(isInternetConnectionActivated()){
+                    //internet is active
+                    proceedLogin();
+                }
+                else{
+                    //internet is not active
+                    new AlertDialog.Builder(LoginActivity.this)
+                            .setTitle("No internet connection!")
+                            .setMessage("Please enable your internet to login!")
+                            // A null listener allows the button to dismiss the dialog and take no further action.
+                            .setPositiveButton("Okay", null)
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+                }
             }
         });
     }
 
     /**
+     * Check internet connection
+     */
+    private boolean isInternetConnectionActivated() {
+        boolean connected = false;
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+            //we are connected to a network
+            connected = true;
+        }
+        else{
+            //we are not connected to a network
+            connected = false;
+        }
+
+        return connected;
+    }
+
+    /**
+     * proceedLogin
+     */
+    private void proceedLogin(){
+        final String name = name_et.getText().toString();
+        final String password = password_et.getText().toString();
+
+        //reference to all the ID's of USER
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                boolean login_successful = false;
+                boolean user_has_nickname = true;
+                //iterate through ID elements of USER
+                for (DataSnapshot child : snapshot.getChildren()) {
+                    //examine if user credentials are correct
+                    if((child.child("user_name").getValue().toString().equals(name)) && (child.child("password").getValue().toString().equals(password))){
+                        login_successful = true;
+                        currentUser = child.getKey().toString();
+
+                        if(child.child("nickname").getValue().equals("")){
+                            user_has_nickname = false;
+                        }
+                    }
+                }
+
+                if(login_successful){
+                    //if login was successful
+                    if(user_has_nickname){
+                        loginSuccessful();
+                    }
+                    else {
+                        openNicknameDialogFragment();
+                    }
+                }
+                else
+                {
+                    //if login failed
+                    Toast.makeText(LoginActivity.this, "Wrong name or password!", Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    /**
+     * apply nickname on dialog pressed OK
+     */
+    @Override
+    public void applyNickname(String nickname) {
+        this.nickname = nickname;
+        reference.child(currentUser).child("nickname").setValue(nickname);
+        loginSuccessful();
+    }
+
+    /**
      * login successful
      */
-    private void LoginSuccessful(){
+    private void loginSuccessful(){
         Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
         openNextActivity();
     }
 
     /**
+     * open nickname dialog fragment
+     */
+    private void openNicknameDialogFragment(){
+        NicknameDialogFragment nicknameDialogFragment = new NicknameDialogFragment();
+        nicknameDialogFragment.show(getSupportFragmentManager(),"nicknameDialogFragment");
+    }
+
+    /**
      * open forgot password fragment
      */
-    protected void openForgotPasswordFragment(){
+    private void openForgotPasswordFragment(){
 
     }
 
     /**
      * open register activity
      */
-    protected void openRegisterActivity(){
+    private void openRegisterActivity(){
 
     }
 
@@ -140,5 +217,4 @@ public class LoginActivity extends AppCompatActivity {
     protected void openNextActivity(){
 
     }
-
 }
